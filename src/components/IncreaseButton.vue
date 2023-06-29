ee00a8f1cf6ab9bb1ccaceceafa211949d69690d
Aierse <68111814+Aierse@users.noreply.github.com>
2023-06-29 10:26:55 +0900

    Initial commit

---
+++ src/components/IncreaseButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="increment">{{ title }}: {{ count }}</button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{
+  title: string
+}>()
+
+const count = ref(0)
+function increment() {
+  count.value++
+}
+</script>
+
+<style scoped></style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import IncreaseButton from '../components/IncreaseButton.vue'
+
+export default {
+  components: {
+    IncreaseButton
+  }
+}
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <IncreaseButton title="First" />
+    <IncreaseButton title="Second" />
+    <IncreaseButton title="Third" />
+    <IncreaseButton title="Fourth" />
+    <IncreaseButton title="Fifth" />
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
