94002e3d4dddae3b671773b27019b83243fd8e3f
Aierse <68111814+Aierse@users.noreply.github.com>
2023-06-30 11:10:59 +0900

    Feat: Body 레이아웃

--- src/components/layouts/BodyLayout.vue
+++ src/components/layouts/BodyLayout.vue
@@ -5,8 +5,11 @@ import SideBar from '../SideBar.vue'
 
 <template>
   <main>
-    <RouterView />
-    <SideBar />
+    <span class="vertical"></span>
+    <article>
+      <RouterView />
+    </article>
+    <span class="vertical"></span>
   </main>
 </template>
 
@@ -14,7 +17,18 @@ import SideBar from '../SideBar.vue'
 main {
   display: flex;
   justify-content: center;
+  margin: 4rem 0;
+}
 
-  margin: 4rem 40rem;
+article {
+  width: 50%;
+  text-align: center;
+}
+
+.vertical {
+  display: inline-block;
+  width: 0.1rem;
+
+  background: linear-gradient(white, 8%, var(--main-color) 84%, white);
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts"></script>
 
 <template>
-  <article>
-    <h1>설명</h1>
-  </article>
+  <h1>설명</h1>
+  <h1>설명</h1>
+  <h1>설명</h1>
+  <h1>설명</h1>
+  <h1>설명</h1>
+  <h1>설명</h1>
+  <h1>설명</h1>
+  <h1>설명</h1>
+  <h1>설명</h1>
+  <h1>설명</h1>
+  <h1>설명</h1>
 </template>
 
-<style>
-article {
-  width: 100%;
-
-  text-align: center;
-}
-</style>
+<style></style>
