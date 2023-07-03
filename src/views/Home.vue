166023b82beda5a9b5bc4efc01db1b8b668928bf
Aierse <68111814+Aierse@users.noreply.github.com>
2023-07-03 09:32:14 +0900

    Feat: md파일 불러오기 성공

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,19 @@
-<script setup lang="ts"></script>
+<script>
+import VueMarkdownIt from 'vue3-markdown-it'
+import test from '../assets/posts/a.md?raw'
+
+export default {
+  components: {
+    VueMarkdownIt
+  },
+
+  data() {
+    return {
+      test: test
+    }
+  }
+}
+</script>
 
 <template>
   <h1>설명</h1>
@@ -12,6 +27,7 @@
   <h1>설명</h1>
   <h1>설명</h1>
   <h1>설명</h1>
+  <VueMarkdownIt class="md-body" :source="test" />
 </template>
 
 <style></style>
