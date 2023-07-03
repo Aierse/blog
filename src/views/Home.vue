0bd91eb7c5cf8bd407caa65815c019892d8de59d
Aierse <68111814+Aierse@users.noreply.github.com>
2023-07-03 13:24:50 +0900

    Feat: Post 불러오기

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,27 @@
-<script>
-import VueMarkdownIt from 'vue3-markdown-it'
-import test from '../assets/posts/a.md?raw'
+<script setup lang="ts">
+const posts = import.meta.glob('../assets/posts/*')
 
-export default {
-  components: {
-    VueMarkdownIt
-  },
+const fileList: string[] = []
 
-  data() {
-    return {
-      test: test
-    }
-  }
+for (const t in posts) {
+  fileList.push(t.split('/').pop()?.replace('.md', '') as string)
 }
 </script>
 
 <template>
-  <h1>설명</h1>
-  <h1>설명</h1>
-  <h1>설명</h1>
-  <h1>설명</h1>
-  <h1>설명</h1>
-  <h1>설명</h1>
-  <h1>설명</h1>
-  <h1>설명</h1>
-  <h1>설명</h1>
-  <h1>설명</h1>
-  <h1>설명</h1>
-  <VueMarkdownIt class="md-body" :source="test" />
+  <section v-for="name in fileList">
+    <h3>
+      <RouterLink :to="'/detail/' + name">{{ name }}</RouterLink>
+    </h3>
+  </section>
 </template>
 
-<style></style>
+<style scoped>
+section:not(:last-child) {
+  margin-bottom: 0.8rem;
+}
+
+h3 {
+  padding: 0.8rem;
+}
+</style>
